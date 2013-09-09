e682f20db148652dc900e550d7f0811142729846
Jeffrey Mullins <jmullins@techresidents.com>
2013-09-09 12:02:45 -0400

    Bumping version to 0.5-SNAPSHOT

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ ez_setup.use_setuptools()
 
 setup(
     name='trrackspace',
-    version = '0.4.0',
+    version = '0.5-SNAPSHOT',
     author = 'Tech Residents, Inc.',
     packages = find_packages(),
     license = open('LICENSE').read(),
